ba4faea93785e9ec9735041e765e5442245b29e2
lichenyang5 <lichenyang0916@gmail.com>
2026-06-15 19:43:04 +0800

    配置路由以及初始化页面结构加导航

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,6 +1,9 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { hapPlugin } from '@hadss/hmrouter-plugin';
 
 export default {
   system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [
+    hapPlugin()      /* HMRouter 编译期插件：扫描 @HMRouter 装饰器生成路由表 */
+  ]
 }
--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,6 +1,9 @@
+import { ascfPlugin } from '@atomicservice/ascf-toolkit-hvigor-plugin';
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
   system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [
+    ascfPlugin()
+  ]       /* Custom plugin to extend the functionality of Hvigor. */
 }
